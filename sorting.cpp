817c39e923ef786771dcf0f11f8a5878008bfa2a
samkitjain <jain.samkit1989@gmail.com>
2024-08-28 22:57:13 -0700

    Bug fix in merge sort

--- sorting.cpp
+++ sorting.cpp
@@ -62,21 +62,21 @@ class quicksort:public sortingutils<CONTAINER_T>, public sortingalgosI<CONTAINER
         quicksort(vector<CONTAINER_T> vec):sortingutils<CONTAINER_T>(vec){}
         void sort();
     private:
-        void qsort(int lo, int hi);
+        void qsort_r(int lo, int hi);
         int partition(int i, int j);
 };
 
 template <typename CONTAINER_T>
 void quicksort<CONTAINER_T>::sort(){
-    this->qsort(0,this->container.size()-1);
+    this->qsort_r(0,this->container.size()-1);
 }
 
 template <typename CONTAINER_T>
-void quicksort<CONTAINER_T>::qsort(int lo, int hi){
+void quicksort<CONTAINER_T>::qsort_r(int lo, int hi){
     if(lo<hi){
         int j = this->partition(lo, hi);
-        this->qsort(lo,j);
-        this->qsort(j+1,hi);
+        this->qsort_r(lo,j-1);
+        this->qsort_r(j+1,hi);
     }
 }
 
@@ -87,12 +87,12 @@ int quicksort<CONTAINER_T>::partition(int lo, int hi){
     int pivot = this->container[lo];
 
     while(i<j){
-        while(this->container[i]<=pivot && i<=hi)i++;
+        while(this->container[i]<=pivot && i<hi)i++;
         while(this->container[j]>pivot && j>=lo)j--;
 	if(i<j)
             this->swap(this->container[j],this->container[i]);
     }
-    this->swap(this->container[j],this->container[lo]);
+    this->swap(this->container[j],this->container[lo]/*aka pivot*/);
     return j;
 }
 
@@ -100,12 +100,72 @@ int quicksort<CONTAINER_T>::partition(int lo, int hi){
 template <typename CONTAINER_T>
 class mergesort:public sortingutils<CONTAINER_T>, public sortingalgosI<CONTAINER_T>{ 
     public: 
-        void sort(){}
+        mergesort(vector<CONTAINER_T> vec):sortingutils<CONTAINER_T>(vec){}
+        void sort();
+    private:
+        void msort(int lo, int hi);
+        void merge(int lo, int mid, int hi);
 };
+
+
+template <typename CONTAINER_T>
+void mergesort<CONTAINER_T>::sort(){
+    msort(0,this->container.size()-1);
+}
+
+template <typename CONTAINER_T>
+void mergesort<CONTAINER_T>::msort(int lo,int hi){
+    if(lo<hi){
+        int mid = (lo+hi)/2;
+	msort(lo,mid);
+	msort(mid+1,hi);
+	merge(lo,mid,hi);
+    }
+}
+
+template <typename CONTAINER_T>
+void mergesort<CONTAINER_T>::merge(int lo, int mid, int hi){
+    int m = mid-lo+1;
+    int n = hi - mid;
+
+    vector<CONTAINER_T> tmp(m+n);
+   
+    // Create 3 iterator i (for lo-mid) , j (for mid+1 , hi) and 
+    // k (intermediate storage)
+    int i = lo;
+    int j = mid+1;
+    int k = 0;
+
+    // Advance pointers till i or j hasn't crossed over its bounds
+    while (i<=mid && j<=hi){
+        // Check if vec[i] >= vec[j] : If yes, copy vec[i] value into tmp[k] and advance i and k
+        if(this->container[i]>= this->container[j]){ // For ascending order container[lo+i] < container[mid+j]
+	    tmp[k] = this->container[i];
+	    i++;
+	    k++;
+	}
+        // Check if vec[i] < vec[j] : If yes, copy vec[j] value into tmp[k] and advance j and k
+        else{
+	    tmp[k] = this->container[j];
+	    j++;
+	    k++;
+	}
+    }
+
+    // Copy remaining elements of 2 sub arrays of container into tmp
+    for(;i<=mid;i++) tmp[k++] = this->container[i];
+    for(;j<=hi;j++) tmp[k++] = this->container[j];
+
+    // Copy tmp vector back into original vector
+    for(int idx = 0 ; idx < tmp.size(); idx++)
+        this->container[lo+idx] = tmp[idx];
+}
 
 int main(void){
 
-   vector<int> v = {10,9,8,7,6,5,4,3,2,1};
+   //vector<int> v = {10,9,8,7,6,5,4,3,2,1};
+   //vector<int> v = {3,2,3,1,2,4,5,5,6};
+   vector<int> v = {1,2,3,4,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,-5,-4,-3,-2,-1};
    quicksort<int> qs(v);
    cout<<"Array before sorting:"<<endl;
    qs.printcontainer();
@@ -113,5 +173,13 @@ int main(void){
    cout<<"Array after sorting:"<<endl;
    qs.printcontainer();
 
-    return 0;
+   vector<int> v1 = {1,2,3,4,5,6,7,8,9,10};
+   mergesort<int> ms(v1);
+   cout<<"Array before sorting:"<<endl;
+   ms.printcontainer();
+   ms.sort();
+   cout<<"Array after sorting:"<<endl;
+   ms.printcontainer();
+
+   return 0;
 }
